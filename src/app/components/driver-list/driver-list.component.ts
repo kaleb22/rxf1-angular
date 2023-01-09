70b022d7951dfcd1624f0e966be9a4680f6f49b0
kaleb22 <kaleb22@hotmail.com>
2023-01-09 11:38:33 -0300

    fix: updating data and pagination bind to matTable when new data come from observable

--- src/app/components/driver-list/driver-list.component.ts
+++ src/app/components/driver-list/driver-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { MatOptionSelectionChange } from '@angular/material/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -10,32 +10,47 @@ import { map, tap } from 'rxjs';
   selector: 'app-driver-list',
   templateUrl: './driver-list.component.html',
   styleUrls: ['./driver-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DriverListComponent {
 
+  constructor(private cdRef: ChangeDetectorRef, private driverService: DriversService) {
+    this.seasons = ['2018', '2019', '2020', '2021', '2022'];
+  }
+
   seasons: string[];
   seasonSelected: string;
-  driverList$ = this.driverService.driverList$.pipe(
-    map( drivers => this.drivers = drivers as IDriver[]),
-    tap( drivers => console.log(drivers))
-  );
   drivers: IDriver[];
   columnsToDisplay = ['name', 'nationality', 'birthday'];
   dataSource: MatTableDataSource<IDriver>;
 
-  private paginator: MatPaginator;
+  driverList$ = this.driverService.driverList$.pipe(
+    map( drivers =>  this.drivers = drivers as IDriver[] ),
+    tap( drivers =>  { 
+      console.log(drivers)
+      this.updateMatTableData();
+    })
+  );
 
+  private paginator: MatPaginator;
+  /*
+   We are using @ViewChild because of the ngIf with async pipe on the template
+   When the data is emmited for the first time from the observable, we need to bind the
+   pagination component with the datasource. This can be achieved with the code bellow.
+  */
   @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
     if(mp) {
       this.paginator = mp;
       this.dataSource = new MatTableDataSource<IDriver>(this.drivers);
       this.dataSource.paginator = this.paginator;
+      this.cdRef.detectChanges();
     }
   }
 
-  constructor(private driverService: DriversService) {
-    this.seasons = ['2018', '2019', '2020', '2021', '2022'];
+  updateMatTableData(): void {
+    if(this.paginator) {
+      this.dataSource = new MatTableDataSource<IDriver>(this.drivers);
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
   /* the behaviour of onSelectionChange is the following:
