c2549ca018cf46a66052840dce485c0b1486dc91
kaleb22 <kaleb22@hotmail.com>
2023-01-20 11:12:13 -0300

    refac: removing mat table from results and  styling

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,6 +1,4 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, Input } from '@angular/core';
 import { IResult } from 'src/app/model/iresult';
 
 @Component({
@@ -8,32 +6,10 @@ import { IResult } from 'src/app/model/iresult';
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent implements AfterViewInit  {
+export class ResultsComponent {
 
   constructor() { }
 
   @Input() title: string;
-  @Input() set setResults(results: IResult[]) {
-    this.results = results;
-    this.dataSource = new MatTableDataSource<IResult>(results);
-    this.dataSource.paginator = this.paginator;
-
-    if(results[0].points) {
-      this.showStandings = true;
-    } else {
-      this.showStandings = false;
-    }
-  };
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  columnsStanding = ['points', 'position', 'driver-name', 'constructor']
-  columnsToDisplay = ['position', 'driver-name', 'constructor'];
-  dataSource: MatTableDataSource<IResult>;
-  results: IResult[];
-  showStandings: boolean;
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-  }
+  @Input() results: IResult[];
 }
