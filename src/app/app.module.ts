068de61ac89e60a11792a93295ebb33bf9f3dbed
kaleb22 <kaleb22@hotmail.com>
2023-07-29 16:09:53 -0300

    feat: creating header component and refactoring its layout. also implementing menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { NgHttpCachingModule, NgHttpCachingConfig, NgHttpCachingStrategy } from 'ng-http-caching';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { DriverListComponent } from './components/driver-list/driver-list.component';
@@ -20,6 +23,7 @@ import { RaceDetailsComponent } from './components/race-details/race-details.com
 import { ResultsComponent } from './components/results/results.component';
 import { DriverComponent } from './components/driver/driver.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 const ngHttpCachingConfig: NgHttpCachingConfig = {
@@ -38,6 +42,7 @@ const ngHttpCachingConfig: NgHttpCachingConfig = {
     ResultsComponent,
     DriverComponent,
     WelcomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,7 +56,10 @@ const ngHttpCachingConfig: NgHttpCachingConfig = {
     MatTableModule,
     MatPaginatorModule,
     NgHttpCachingModule.forRoot(ngHttpCachingConfig),
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
