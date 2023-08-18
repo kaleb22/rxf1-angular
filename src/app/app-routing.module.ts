4bf3e90f9ddf03a18e3a93cb9fb5387f07263417
kaleb22 <kaleb22@hotmail.com>
2023-08-18 17:02:20 -0300

    refac: removing old components not being used anymore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DriverListComponent } from './components/driver-list/driver-list.component';
-import { RaceListComponent } from './components/race-list/race-list.component';
-import { RaceListWrapperComponent } from './components/race-list-wrapper/race-list-wrapper.component';
-import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { DriverComponent } from './components/driver/driver.component';
+import { RaceComponent } from './components/race/race.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent},
   { path: 'drivers', component: DriverComponent },
-  { path: 'races', component: RaceListWrapperComponent }
+  { path: 'races', component: RaceComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DriverListComponent } from './components/driver-list/driver-list.component';
-import { RaceListComponent } from './components/race-list/race-list.component';
-import { RaceListWrapperComponent } from './components/race-list-wrapper/race-list-wrapper.component';
-import { RaceDetailsComponent } from './components/race-details/race-details.component';
 import { ResultsComponent } from './components/results/results.component';
 import { DriverComponent } from './components/driver/driver.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
@@ -40,10 +36,6 @@ const ngHttpCachingConfig: NgHttpCachingConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    DriverListComponent,
-    RaceListComponent,
-    RaceListWrapperComponent,
-    RaceDetailsComponent,
     ResultsComponent,
     DriverComponent,
     WelcomeComponent,
