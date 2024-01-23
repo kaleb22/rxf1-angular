d1a67e1855294f90adfa06f718f10960aa40b91e
kaleb22 <kaleb22@hotmail.com>
2024-01-23 15:07:53 -0300

    feat: lazy-loading the drivers and races components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { DriverComponent } from './components/driver/driver.component';
-import { RaceComponent } from './components/race/race.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent},
-  { path: 'drivers', component: DriverComponent },
-  { path: 'races', component: RaceComponent }
+  {
+    path: 'drivers',
+    loadComponent: () =>
+      import('./components/driver/driver.component').then(m => m.DriverComponent)
+  },
+  { path: 'races',
+    loadComponent: () =>
+      import('./components/race/race.component').then(m => m.RaceComponent)
+  }
 ]
 
 @NgModule({
