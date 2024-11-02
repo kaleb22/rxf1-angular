040d329fabef852e32300fdfb1217abf036dd07d
kaleb22 <kaleb22@hotmail.com>
2024-11-02 17:39:07 -0300

    chore: update project to angular v18

--- src/app/services/drivers.service.spec.ts
+++ src/app/services/drivers.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { DriversService } from './drivers.service';
