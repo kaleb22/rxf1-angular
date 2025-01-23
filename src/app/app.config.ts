42b7870efd40f9f68acf7214ae4568c0181608e4
kaleb22 <kaleb22@hotmail.com>
2025-01-23 14:30:36 -0300

    feat(error-interceptor): creating interceptor to handle errors globally

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,32 @@
 import { ApplicationConfig } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { NgHttpCachingConfig, NgHttpCachingModule, NgHttpCachingStrategy } from 'ng-http-caching';
+import {
+  NgHttpCachingConfig,
+  NgHttpCachingModule,
+  NgHttpCachingStrategy,
+} from 'ng-http-caching';
 import { routes } from './app.routes';
 import { MatDialogModule } from '@angular/material/dialog';
+import { errorInterceptor } from './interceptors/error.interceptor';
 
 const ngHttpCachingConfig: NgHttpCachingConfig = {
   lifetime: 1000 * 60 * 10, // cache expire after 10 min,
   allowedMethod: ['GET', 'HEAD'],
-  cacheStrategy: NgHttpCachingStrategy.ALLOW_ALL
+  cacheStrategy: NgHttpCachingStrategy.ALLOW_ALL,
 };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
-    importProvidersFrom(NgHttpCachingModule.forRoot(ngHttpCachingConfig), MatDialogModule)
-  ]
+    importProvidersFrom(
+      NgHttpCachingModule.forRoot(ngHttpCachingConfig),
+      MatDialogModule,
+    ),
+  ],
 };
---
+++ src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((err: HttpErrorResponse) => {
+      let errorMessage = '';
+      if (err.error instanceof ErrorEvent) {
+        errorMessage = `An error occurred: ${err.error.message}`;
+      } else {
+        errorMessage = `Server returned code: ${err.status}, error message is: ${
+          err.message
+        }`;
+      }
+      console.error(errorMessage);
+      return throwError(() => new Error(errorMessage));
+    }),
+  );
+};
