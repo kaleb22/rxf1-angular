fe88c42c09d403983e6935342ea76500bca8fde0
kaleb22 <kaleb22@hotmail.com>
2025-01-24 17:59:13 -0300

    fix: tests

--- src/app/components/race/race.component.spec.ts
+++ src/app/components/race/race.component.spec.ts
@@ -4,26 +4,19 @@ import { provideHttpClient } from '@angular/common/http';
 import { SpinnerService } from './../../services/spinner.service';
 import { RacesService } from './../../services/races.service';
 import { RaceComponent } from './race.component';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RaceComponent', () => {
   let component: RaceComponent;
   let fixture: ComponentFixture<RaceComponent>;
-  let racesService: RacesService;
-  let spinnerService: SpinnerService;
-  let matDialog: MatDialog;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RaceComponent, MatDialogModule, BrowserAnimationsModule],
-      providers: [RacesService, SpinnerService, provideHttpClient()]
-    })
-    .compileComponents();
+      providers: [RacesService, SpinnerService, provideHttpClient()],
+    }).compileComponents();
 
-    racesService = TestBed.inject(RacesService);
-    spinnerService = TestBed.inject(SpinnerService);
-    matDialog = TestBed.inject(MatDialog);
     fixture = TestBed.createComponent(RaceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/welcome/welcome.component.spec.ts
+++ src/app/components/welcome/welcome.component.spec.ts
@@ -1,20 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WelcomeComponent } from './welcome.component';
-import { Router } from '@angular/router';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
-  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WelcomeComponent]
-    })
-    .compileComponents();
+      imports: [WelcomeComponent],
+    }).compileComponents();
 
-    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
