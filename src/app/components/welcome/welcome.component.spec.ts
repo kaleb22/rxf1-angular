bc6c55b0c61933af29342391b5b3ea48f087dd4d
kaleb22 <kaleb22@hotmail.com>
2025-01-27 16:31:11 -0300

    feat(welcome-component): impl test

--- src/app/components/welcome/welcome.component.spec.ts
+++ src/app/components/welcome/welcome.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 import { WelcomeComponent } from './welcome.component';
 
@@ -9,6 +11,7 @@ describe('WelcomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [WelcomeComponent],
+      providers: [Router],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WelcomeComponent);
@@ -18,5 +21,16 @@ describe('WelcomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should go to drivers page when btn is clicked', () => {
+    const spyGoToDrivers = jest.spyOn(component, 'goToDriversPage');
+    const btn = fixture.debugElement.query(
+      By.css('[data-testId="main-btn"]'),
+    ).nativeElement;
+    btn.click();
+    fixture.detectChanges();
+
+    expect(spyGoToDrivers).toHaveBeenCalled();
   });
 });
