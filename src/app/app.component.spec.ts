d25feaddd5c5205c5d3ace70163ae783abe78084
kaleb22 <kaleb22@hotmail.com>
2025-01-31 13:01:31 -0300

    feat(app-component): impl test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppComponent],
+      providers: [Router],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -21,5 +25,12 @@ describe('AppComponent', () => {
 
   it(`should have as title 'rxf1-angular'`, () => {
     expect(component.title).toEqual('rxf1-angular');
+  });
+
+  it('should trigger router events subscription', () => {
+    router.navigateByUrl('');
+    fixture.detectChanges();
+
+    expect(component.activeLinkIndex).toBe(-1);
   });
 });
